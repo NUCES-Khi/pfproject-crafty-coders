4df461218d80900a94c80bbda615d3a4ecd60f05
layyana-junaid <142867946+layyana-junaid@users.noreply.github.com>
2023-12-05 23:33:58 +0500

    Update healthgear.c

--- healthgear.c
+++ healthgear.c
@@ -69,6 +69,56 @@ void cough_function(int coughOption, int age) {
     } 
 } // end of cough function 
 
+void BloodPressure(int bpup, int bpdown, char height){
+  printf("If your height is between 0ft - 5ft, enter s.");
+  printf("\n");
+  printf("If your height is between 5ft - 8ft, enter t.");
+  printf("\n");
+  scanf(" %c", &height);
+  printf("What is the systolic blood pressure (upper)");
+  scanf(" %d", &bpup);
+  printf("\n");
+  printf("What is the diastolic blood pressure (lower)");
+  scanf(" %d", &bpdown);
+  if (height == 's') {
+     if (bpup <= 115 && bpup > 80 && bpdown >= 40 && bpdown <= 75) {
+     FILE *fp = fopen("bp1.txt", "r");
+     read_file(fp);
+     }//ending first if
+ else if (bpup <= 130 && bpup >= 116 && bpdown >= 76 && bpdown <= 90 ) {
+   printf("Congratulations! Your blood pressure is normal!");
+   printf("\n");
+   }//ending first else if
+else if (bpup <= 180 && bpup > 131 && bpdown > 90 && bpdown < 110) {
+   FILE *fp = fopen("bp2.txt", "r");
+   read_file(fp);
+   }//ending second else if
+else {
+      printf("Please make sure you have entered the correct values, since the reading you have entered is not possible.");
+      printf("\n");
+     }//ending else
+  }//ending first height if
+if (height == 't') {                
+    if (bpup <= 110 && bpup > 75 && bpdown >= 35 && bpdown <= 70) {
+    FILE *fp = fopen("bp1.txt", "r");
+    read_file(fp);
+    }//ending first if
+else if (bpup <= 125 && bpup >= 111 && bpdown >= 71 && bpdown <= 85 ) {
+     printf("Congratulations! Your blood pressure is normal!");
+     printf("\n");
+     }//ending first else if
+else if(bpup <= 175 && bpup > 125 && bpdown > 85 && bpdown < 105) {
+        FILE *fp = fopen("bp2.txt", "r");
+        read_file(fp);
+      }//ending second else if
+else {
+	printf("Please make sure you have entered the correct values, since the reading you have entered is not possible.");
+        printf("\n");
+     }//ending else
+}//ending second height if
+}//Our bp function has ended here
+
+
 int main(int argc, char const *argv[]) {
 float temp;
 int option; // input from the user, to choose the symptom they want to cater 
